b1290c77e1694b89d2523be0b71afeab9c8ac42b
Jake Soenneker <jake@soenneker.com>
2024-12-22 08:49:37 -0600

    Circ

--- test/Soenneker.Extensions.ValueTask.Tests/ValueTaskExtensionTests.cs
+++ test/Soenneker.Extensions.ValueTask.Tests/ValueTaskExtensionTests.cs
@@ -1,9 +1,8 @@
-using Soenneker.Tests.Unit;
 using Xunit;
 
 namespace Soenneker.Extensions.ValueTask.Tests;
 
-public class ValueTaskExtensionTests : UnitTest
+public class ValueTaskExtensionTests
 {
     [Fact]
     public void Default()
