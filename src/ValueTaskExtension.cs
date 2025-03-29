95177d942e10080b8f0615d1ef30def9d146b164
Jake Soenneker <jake@soenneker.com>
2025-03-29 09:36:47 -0500

    RunSync methods

--- src/ValueTaskExtension.cs
+++ src/ValueTaskExtension.cs
@@ -30,4 +30,35 @@ public static class ValueTaskExtension
     {
         return valueTask.ConfigureAwait(false);
     }
+
+    /// <summary>
+    /// Synchronously runs the specified <see cref="ValueTask{TResult}"/> and returns its result.
+    /// </summary>
+    /// <typeparam name="T">The result type of the <see cref="ValueTask{T}"/>.</typeparam>
+    /// <param name="valueTask">The <see cref="ValueTask{T}"/> to run synchronously.</param>
+    /// <returns>The result of the completed <see cref="ValueTask{T}"/>.</returns>
+    /// <remarks>
+    /// If the <see cref="ValueTask{T}"/> has not yet completed, this method will block the calling thread
+    /// until it does. This may lead to deadlocks if called on a context that does not allow synchronous blocking.
+    /// </remarks>
+    public static T RunSync<T>(this ValueTask<T> valueTask)
+    {
+        return valueTask.IsCompletedSuccessfully
+            ? valueTask.Result
+            : valueTask.AsTask().GetAwaiter().GetResult();
+    }
+
+    /// <summary>
+    /// Synchronously runs the specified <see cref="ValueTask"/>.
+    /// </summary>
+    /// <param name="valueTask">The <see cref="ValueTask"/> to run synchronously.</param>
+    /// <remarks>
+    /// If the <see cref="ValueTask"/> has not yet completed, this method will block the calling thread
+    /// until it does. This may lead to deadlocks if called on a context that does not allow synchronous blocking.
+    /// </remarks>
+    public static void RunSync(this System.Threading.Tasks.ValueTask valueTask)
+    {
+        if (!valueTask.IsCompletedSuccessfully)
+            valueTask.AsTask().GetAwaiter().GetResult();
+    }
 }
